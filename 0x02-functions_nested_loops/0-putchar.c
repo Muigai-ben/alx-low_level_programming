5638cae9bdfb61789b318a42fa947beb0c5ca563
Muigai-ben <muigaibenson37@gmail.com>
2023-06-20 12:00:13 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-	/**
-	 * main - Entry point
-	 * Description: printing _putchar
-	 * Return: Always 0
-	 */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 
 int main(void)
 	{
