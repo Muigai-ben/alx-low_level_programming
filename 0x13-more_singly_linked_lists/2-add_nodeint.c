1eaf36258b3f3152df6d8905eb478a4bd21d51ae
Muigai-ben <muigaibenson37@gmail.com>
2023-08-01 12:17:25 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,11 +3,11 @@
 /**
  * add_nodeint - adds a new node at the beginning of a linked list
  * @head: pointer to the first node in the list
- * @v: data to insert in that new node
+ * @n: data to insert in that new node
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int v)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
@@ -15,7 +15,7 @@ listint_t *add_nodeint(listint_t **head, const int v)
 	if (!new)
 		return (NULL);
 
-	new->v = v;
+	new->n = n;
 	new->next = *head;
 	*head = new;
 
